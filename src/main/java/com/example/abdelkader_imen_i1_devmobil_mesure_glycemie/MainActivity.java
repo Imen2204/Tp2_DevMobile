35b937218793f1958e79191cc511fb10a98d470f
Imen Abdelkader <73885836+Imen2204@users.noreply.github.com>
2024-02-22 22:52:59 +0100

    Add files via upload

---
+++ src/main/java/com/example/abdelkader_imen_i1_devmobil_mesure_glycemie/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.abdelkader_imen_i1_devmobil_mesure_glycemie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Calculer(View view) {
+    }
+}
